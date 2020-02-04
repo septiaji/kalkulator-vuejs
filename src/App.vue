66fea6242f8948ecb1a49f4f32155c7e38420541
septiaji <60614997+septiaji@users.noreply.github.com>
2020-02-04 07:51:55 +0700

    file-src

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Kalkulator/>
+  </div>
+</template>
+
+<script>
+import Kalkulator from './components/Kalkulator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Kalkulator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
